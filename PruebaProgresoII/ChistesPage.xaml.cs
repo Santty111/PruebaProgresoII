930cb60e51810a749db5823ce793e115a2ebc074
Santiago <santtyconla@gmail.com>
2025-06-03 08:38:03 -0500

    Creacion de chistes page

--- PruebaProgresoII/ChistesPage.xaml.cs
+++ PruebaProgresoII/ChistesPage.xaml.cs
@@ -1,3 +1,5 @@
+using System.Net.Http.Json;
+
 namespace PruebaProgresoII;
 
 public partial class ChistesPage : ContentPage
@@ -5,5 +7,31 @@ public partial class ChistesPage : ContentPage
 	public ChistesPage()
 	{
 		InitializeComponent();
-	}
+        LoadJoke();
+    }
+
+    private async void LoadJoke()
+    {
+        try
+        {
+            var httpClient = new HttpClient();
+            var joke = await httpClient.GetFromJsonAsync<Joke>("https://official-joke-api.appspot.com/random_joke");
+            JokeLabel.Text = $"{joke.Setup}\n{joke.Punchline}";
+        }
+        catch
+        {
+            JokeLabel.Text = "Error al obtener el chiste.";
+        }
+    }
+
+    private void Button_ClickedChiste(object sender, EventArgs e)
+    {
+        LoadJoke();
+    }
+}
+
+public class Joke
+{
+    public string Setup { get; set; }
+    public string Punchline { get; set; }
 }
